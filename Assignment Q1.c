a3847717c2185a74e49b21115adf1c6797641941
Vasthrala <128654578+Vasthrala@users.noreply.github.com>
2023-03-24 09:09:31 +0530

    Add files via upload

---
+++ Assignment Q1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int c[]={2.8,3.4,6,6.7,5};
+	int j,*p=c,*q=c;
+	for(j=0;j<5;j++)
+	{
+	printf("%d",*p);
+    ++q; }
+    for(j=0;j<5;j++)
+    {
+    	printf("%d",*p);
+    ++q; }
+	
+}
